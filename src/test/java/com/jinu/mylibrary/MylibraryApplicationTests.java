0e10852acbb482a00105a26f9f05300236658431
parkjinu <jupark@autocrypt.io>
2024-02-20 17:51:14 +0900

    Initial commit

---
+++ src/test/java/com/jinu/mylibrary/MylibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinu.mylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
